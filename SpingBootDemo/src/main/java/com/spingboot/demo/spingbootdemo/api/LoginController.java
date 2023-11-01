96c579cda1bdaaf8a560fa0eee113fcd8ff8dbc8
XieHui <2210313035@qq.com>
2023-11-01 18:07:02 +0800

    x;

--- SpingBootDemo/src/main/java/com/spingboot/demo/spingbootdemo/api/LoginController.java
+++ SpingBootDemo/src/main/java/com/spingboot/demo/spingbootdemo/api/LoginController.java
@@ -26,6 +26,7 @@ public class LoginController {
     private final UserService userService;
     private final RedisService redisService;
 
+
     @Autowired
     public LoginController(UserService userService, RedisService redisService) {
         this.userService = userService;
